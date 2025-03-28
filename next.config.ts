16dcdf98d10609b9af3184b1b106ea8d4cb22262
dreampasssser <dreampasssser@foxmail.com>
2025-03-28 15:26:55 +0800

    添加在cloudflare page上部署的配置。

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,6 +7,10 @@ const nextConfig: NextConfig = {
   // experimental: {
   //   ppr: 'incremental',
   // },
+}
+
+if (process.env.NODE_ENV === 'development') {
+  setupDevPlatform().catch((e) => console.error(e))
 }
 
 export default nextConfig
